a6fdedeff689614355b88dc889082a25d33e00b4
abeidahmed <abeidahmed92@gmail.com>
2021-07-14 23:08:31 +0530

    create user model

--- Gemfile
+++ Gemfile
@@ -3,12 +3,12 @@ git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
 ruby "3.0.1"
 
+gem "bcrypt", "~> 3.1.7"
 gem "bootsnap", ">= 1.4.4", require: false
 gem "pg", "~> 1.1"
 gem "puma", "~> 5.0"
 gem "rails", "~> 6.1.4"
 # gem "redis", "~> 4.0"
-# gem "bcrypt", "~> 3.1.7"
 
 # Use Rack CORS for handling Cross-Origin Resource Sharing (CORS), making cross-origin AJAX possible
 # gem "rack-cors"
---
+++ db/migrate/20210714173609_create_users.rb
@@ -0,0 +1,17 @@
+class CreateUsers < ActiveRecord::Migration[6.1]
+  def change
+    create_table :users do |t|
+      t.string :first_name
+      t.string :last_name
+      t.string :email
+      t.string :password_digest
+      t.string :auth_token
+      t.boolean :admin
+
+      t.timestamps
+    end
+
+    add_index :users, :email, unique: true
+    add_index :users, :auth_token, unique: true
+  end
+end
